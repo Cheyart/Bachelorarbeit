0fc117043728b9b6510508b1bd9e45f0f9c4336c
Catherine <catherine.heyart@student.htw-berlin.de>
2022-05-30 16:21:46 +0200

    added detection of smartphone pitch (rotation along x-axis)

--- HdT_Prototyp/Assets/Scripts/Prototypes/Gyroscope/GyroscopeScript.cs
+++ HdT_Prototyp/Assets/Scripts/Prototypes/Gyroscope/GyroscopeScript.cs
@@ -41,9 +41,14 @@ public class GyroscopeScript : MonoBehaviour
         if (_gyroEnabled)
         {
             guiStyle.fontSize = 40; //change the font size
-            GUI.Label(new Rect(500, 300, 200, 40), "Gyro attitude" + _gyro.attitude, guiStyle);
-            GUI.Label(new Rect(500, 400, 200, 40), "In Euler" + _gyro.attitude.eulerAngles, guiStyle);
-            GUI.Label(new Rect(500, 500, 200, 40), "Gyro enabled : " + _gyro.enabled, guiStyle);
+            GUI.Label(new Rect(200, 300, 200, 40), "Gyro attitude" + _gyro.attitude, guiStyle);
+            GUI.Label(new Rect(200, 400, 200, 40), "In Euler" + _gyro.attitude.eulerAngles, guiStyle);
+            GUI.Label(new Rect(200, 500, 200, 40), "Z Rotation" + GetZRotation(), guiStyle);
+            GUI.Label(new Rect(200, 600, 200, 40), "X Rotation" + GetXRotation(), guiStyle);
+            //GUI.Label(new Rect(200, 500, 200, 40), "X Rotation" + GetXRotation(), guiStyle);
+
+
+            GUI.Label(new Rect(200, 700, 200, 40), "Gyro enabled : " + _gyro.enabled, guiStyle);
             
 
         }
@@ -68,5 +73,39 @@ public class GyroscopeScript : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+
+    float GetZRotation()
+    {
+        Quaternion referenceRotation = Quaternion.identity;
+        Quaternion deviceRotation = GetDeviceRotation();
+        Quaternion eliminationOfXY = Quaternion.Inverse(
+ Quaternion.FromToRotation(referenceRotation * Vector3.forward,
+                                deviceRotation * Vector3.forward)
+  );
+        Quaternion rotationZ = eliminationOfXY * deviceRotation;
+        return rotationZ.eulerAngles.z;
+    }
+
+    float GetXRotation()
+    {
+        Quaternion referenceRotation = Quaternion.identity;
+        Quaternion deviceRotation = GetDeviceRotation();
+        Quaternion eliminationOfYZ = Quaternion.Inverse(
+ Quaternion.FromToRotation(referenceRotation * Vector3.right,
+                                deviceRotation * Vector3.right)
+  );
+        Quaternion rotationX = eliminationOfYZ * deviceRotation;
+        return rotationX.eulerAngles.x;
+    }
+
+    Quaternion GetDeviceRotation()
+    {
+        if(_gyro!= null)
+        {
+            return new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * _gyro.attitude * new Quaternion(0, 0, 1, 0);
+        }
+        return new Quaternion();
     }
 }
