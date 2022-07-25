588b27bc3c1ca7964b6d16c4b5ef5149d582ca4e
Catherine <catherine.heyart@student.htw-berlin.de>
2022-07-25 12:58:45 +0200

    changed icon for user position

---
+++ HdT_Prototyp/Assets/Scripts/FinalPrototype/GUI/Behaviour/Billboard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(_camera.transform, _camera.transform.up);
+    }
+}
