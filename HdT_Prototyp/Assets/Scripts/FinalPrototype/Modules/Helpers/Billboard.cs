fc040d8a823b5d61ccfc23851ce7718eb74ca6d9
Catherine <catherine.heyart@student.htw-berlin.de>
2022-07-29 17:48:49 +0200

    updated Billboard script to fix specific axis

--- HdT_Prototyp/Assets/Scripts/FinalPrototype/Modules/Helpers/Billboard.cs
+++ HdT_Prototyp/Assets/Scripts/FinalPrototype/Modules/Helpers/Billboard.cs
@@ -7,15 +7,60 @@ public class Billboard : MonoBehaviour
     [SerializeField]
     private Camera _camera;
 
+    [SerializeField]
+    private bool _fixXAxis;
+
+    [SerializeField]
+    private bool _fixYAxis;
+
+    [SerializeField]
+    private bool _fixZAxis;
+
+    [SerializeField]
+    private float _xOffset;
+
+    [SerializeField]
+    private float _yOffset;
+
+    [SerializeField]
+    private float _zOffset;
+
+    private Vector3 _multiplicationVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        if (!_fixXAxis)
+        {
+            x = 1;
+        }
+        if (!_fixYAxis)
+        {
+            y = 1;
+        }
+        if (!_fixZAxis)
+        {
+            z = 1;
+        }
+
+        _multiplicationVector = new Vector3(x, y, z);
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         transform.LookAt(_camera.transform, _camera.transform.up);
+
+       
+        
+        transform.localRotation = Quaternion.Euler(Vector3.Scale(transform.localRotation.eulerAngles, _multiplicationVector));
+        Vector3 currentRot = transform.localRotation.eulerAngles;
+        transform.localRotation = Quaternion.Euler(currentRot.x + _xOffset, currentRot.y + _yOffset, currentRot.z + _zOffset);
+        
     }
 }
